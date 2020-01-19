62e732278cc006e25e8f20e8197144bfb4a9c96a
prasadkarkera <prasadkarkera@users.noreply.github.com>
2020-01-19 12:04:06 -0800

    Fix bug

--- SerializeDeserializeBT.py
+++ SerializeDeserializeBT.py
@@ -16,14 +16,27 @@ def serialize(root, queue):
     return queue
 
 
-def deserialize(root, queue, i):
-    if i > len(queue) or queue[i] == -1 or root is None:
+# Global variable to keep iterating through the queue
+i = -1
+def deserialize(root, queue):
+    global i
+    i = i + 1
+    if i > len(queue) or queue[i] == -1:
         return
-
     root.value = queue[i]
-    deserialize(root.left, queue, i + 1)
-    deserialize(root.right, queue, i + 1)
-    return head
+    # We need to create new nodes for the children otherwise you will be passing None in recursion
+    root.left = Node(-1)
+    root.right = Node(-1)
+    
+    deserialize(root.left, queue)
+    deserialize(root.right, queue)
+    
+    # We need to remove nodes that don't have value.
+    if root.left.value == -1:
+        root.left = None
+    if root.right.value == -1:
+        root.right = None
+    return root
 
 head = Node(26)
 head.left = Node(11)
@@ -37,4 +50,4 @@ print(serialize(head, []))
 
 new_head = Node(0)
 
-print(deserialize(new_head, serialize(head, []), 0))
+print(serialize(deserialize(new_head, serialize(head, [])), []))
